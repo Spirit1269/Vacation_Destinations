6f322785623d611f641d73061c23191deefea059
Spirit1269 <kimberlydjenkins1@gmail.com>
2023-04-13 14:46:45 -0500

    First Commit with complete migration and seed files

---
+++ migration.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS destinations CASCADE;
+
+
+CREATE TABLE destinations (
+    id SERIAL PRIMARY KEY,
+    country VARCHAR(200),
+    city_state VARCHAR(100),
+    climate (text)
+);
---
+++ seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO destinations (country, city_state, climate) VALUES ('US', 'Miami, FL', 'warm')
+INSERT INTO destinations (country, city_state, climate) VALUES ('US', 'Santa Monica, CA', 'warm')
+INSERT INTO destinations (country, city_state, climate) VALUES ('US', 'Puerto Rico', 'warn')
+INSERT INTO destinations (country, city_state, climate) VALUES ('US', 'Alaska', 'cold')
+INSERT INTO destinations (country, city_state, climate) VALUES ('South Africa,' 'Cape Town', 'seasonal')
